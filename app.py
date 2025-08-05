b6838053a2cfb9026c87e47ae9636e47b1664ebd
iamyasodapuli <yasodapulicsk7@gmail.com>
2025-08-05 21:39:46 +0530

    mutliplication app.py

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ num2 = 6.3
 sum = num1 + num2
 #sub two num
 sub =num2- num1
-
+#mul of two numbers
 mul= num1*num2
-# Display the sum
+# Display the sum sub mul
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 print('The sub of {0} and {1} is {2}'.format(num1, num2, sub))
+print('The mul of {0} and {1} is {2}'.format(num1, num2, mul))
